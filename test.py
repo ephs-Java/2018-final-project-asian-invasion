bacf4655d86b3763cfec65e84772f49504a550ff
90302673 <90302673@hs-03721-macair.ad.edenpr.org>
2018-05-22 13:10:00 -0500

    fix git ignore, add test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.make_widgets()
+
+    def make_widgets(self):
+        lbl = Label(root, text="Username")
+        lbl.pack()
+        ent = Entry(root)
+        ent.pack()
+        lbl2 = Label(root, text="Password")
+        lbl2.pack()
+        ent2 = Entry(root)
+        ent2.pack()
+        btn = Button(root, text="Log In")
+        btn.pack()
+
+
+root = Tk()
+app = Window(root)
+root.mainloop()

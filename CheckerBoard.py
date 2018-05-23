d9fcc22036fb6f22d577fb8131284b701a612d4e
90305673 <90305673@ep-student.org>
2018-05-23 12:34:07 -0500

    HI

--- CheckerBoard.py
+++ CheckerBoard.py
@@ -2,29 +2,29 @@ from tkinter import *
 
 root = Tk()
 def boardSetter():
-    board={}
-    for row in range(0,9):
-        for col in range(0,9):
+    Matrix = [[0 for x in range(8)] for y in range(8)]
+    for row in range(0,8):
+        for col in range(0,8):
             if row<4:
                 if (row+col)%2==1:
-                    board[row,col]=1
+                    Matrix[row][col] = 1
             elif row>5:
                 if (row+col)%2==1:
-                    board[row, col] = 2
-    return board
+                    Matrix[row][col] = 2
+    return Matrix
 
 canvas = Canvas(root, width=400, height=400)
 canvas.pack()
-board= boardSetter()
+board = boardSetter()
 for row in range(0,9):
     for col in range(0,9):
         if (row+col)%2 == 0:
             canvas.create_rectangle(row*50, col*50, (row+1)*50, (col+1)*50, fill="black")
         else:
             canvas.create_rectangle(row*50, col*50, (row+1)*50, (col+1)*50, fill="gray85")
-        if board[row, col] == 1:
+        if board[row][col] == 1:
             canvas.create_oval(row * 50, col * 50, (row + 1) * 50, (col + 1) * 50, fill="red")
-        elif board[row, col] == 2:
+        elif board[row][col] == 2:
             canvas.create_oval(row * 50, col * 50, (row + 1) * 50, (col + 1) * 50, fill="cyan")
 
 

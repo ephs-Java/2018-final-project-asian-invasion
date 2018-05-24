027f99d3b848772fa9a213daff2728ea2197f9ef
James Cross <90302663@ep-student.org>
2018-05-24 15:16:35 -0500

    Working AI for diabetes data

--- KerasTesting.py
+++ KerasTesting.py
@@ -26,13 +26,3 @@ with open("model.json", "w") as json_file:
 model.save_weights("model.h5")
 print("Saved model to disk")
 
-# load json and create model
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("model.h5")
-print("Loaded model from disk")
-
-predictions = model.predict(X)

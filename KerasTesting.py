eda34542bfddf75a880a6b5a72a62569ecb7241d
James Cross <90302663@ep-student.org>
2018-05-24 12:38:33 -0500

    Working AI for diabetes data

---
+++ KerasTesting.py
@@ -0,0 +1,38 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import model_from_json
+import numpy as np
+
+np.random.seed(7)
+
+dataset = np.loadtxt("pima-indians-diabetes.csv", delimiter=",")
+X = dataset[:,0:8]
+Y = dataset[:,8]
+
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(X, Y, epochs=150, batch_size=10)
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
+
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+predictions = model.predict(X)
